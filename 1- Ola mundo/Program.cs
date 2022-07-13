c13b60f0ac64bea2e18cc502649dfd6b8ae69d6b
bruno <bruno@DESKTOP-EL5KK3U>
2022-07-13 17:33:15 -0300

    Adicionar arquivos de projeto.

---
+++ 1- Ola mundo/Program.cs
@@ -0,0 +1,11 @@
+﻿class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, mundo!");
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.WriteLine();
+    }
+}
+
+
